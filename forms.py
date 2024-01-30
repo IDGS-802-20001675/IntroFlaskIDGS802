efbc0af6f0113c3a6256b41a7b77035db67711af
vanessir <vanesolis929@gmail.com>
2024-01-29 22:53:24 -0600

    VFS Macros IDGS802

--- forms.py
+++ forms.py
@@ -1,12 +1,10 @@
-from wtforms import format
-from wtforms import StringField, TextAreaField, SelectField,RadioField
+from wtforms import Form
+from wtforms import StringField, TextAreaField,SelectField,RadioField
 from wtforms import EmailField
-
 
 class UserForm(Form):
     nombre=StringField("nombre")
-    email=EmailField("email")
-    apaterno=TextAreaField("apaterno")
-    materias=SelectField(choices=[('Español', 'Esp',),('Mat', 'Matematicas'),
-                                  ('Ingles', 'ING')])
-    radios=RadioField('Curso', choices=[("1", "1"),("2", "2"),("3", "3")])
+    email=EmailField("correo")
+    apaterno=StringField("apaterno")
+    materias=SelectField(choices=[('Español','esp'),('Matematicas','mat'), ('Ingles','ING') ])
+    radios=RadioField('Curso',choices=[('1','1'),('2','2'),('3','3')])
